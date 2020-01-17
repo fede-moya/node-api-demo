f27688cde568c73c04bf4d19d1b95fedf5f09a82
Federico Moya <federicomoyamartin@gmail.com>
2020-01-17 01:01:19 -0300

    wip

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
+var os = require('os');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('<h1> Hello from host: ' + os.hostname() + '</h1>');
 });
 
 app.listen(3000, function () {
--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 'use strict';
 
 const express = require('express');
+const os = require('os');
 
 // Constants
-const PORT = 8080;
+const PORT = process.env.BACKEND_PORT || 8080;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('<h1> Hello from host: ' + os.hostname() + '</h1>');
 });
 
 app.listen(PORT, HOST);
